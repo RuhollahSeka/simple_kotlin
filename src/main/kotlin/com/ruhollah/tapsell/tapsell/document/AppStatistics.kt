b38868b30cf764cbadde99bc5a5b8bf496fb62d6
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2019-05-30 15:08:46 +0430

    Complete document and repository

--- src/main/kotlin/com/ruhollah/tapsell/tapsell/document/AppStatistics.kt
+++ src/main/kotlin/com/ruhollah/tapsell/tapsell/document/AppStatistics.kt
@@ -2,34 +2,20 @@ package com.ruhollah.tapsell.tapsell.document
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.io.Serializable
 import java.util.*
 
 @Document
 class AppStatistics(@Id val id: AppStatisticsKey,
-                    val videoRequests: Int,
-                    val webViewRequest: Int,
-                    val videoClicks: Int,
-                    val webViewClicks: Int,
-                    val videoInstalls: Int,
-                    val webViewInstalls: Int)
+                    var videoRequests: Int,
+                    var webViewRequest: Int,
+                    var videoClicks: Int,
+                    var webViewClicks: Int,
+                    var videoInstalls: Int,
+                    var webViewInstalls: Int) : Serializable
 {
-//    @Id
-//    val id: AppStatisticsKey
-//
-//    constructor(id: String, reportTime: Date, type: Int, videoRequests: Int, webViewInstalls: Int, videoClicks: Int,
-//                webViewClicks: Int, videoInstalls: Int, webViewInstalls: Int)
-//    {
-//        this.id = AppStatisticsKey(id, reportTime, type)
-//
-//    }
-//
-//    init
-//    {
-//        this.id = AppStatisticsKey(id, reportTime, type)
-//
-//    }
 
     class AppStatisticsKey(val id: String,
                            val reportTime: Date,
-                           val type: Int)
+                           val type: Int) : Serializable
 }
--- src/main/kotlin/com/ruhollah/tapsell/tapsell/repository/AppStatisticsRepository.kt
+++ src/main/kotlin/com/ruhollah/tapsell/tapsell/repository/AppStatisticsRepository.kt
@@ -8,7 +8,7 @@ import java.util.*
 @Repository
 interface AppStatisticsRepository : MongoRepository<AppStatistics, AppStatistics.AppStatisticsKey>
 {
-    fun findById_ReportTimeBetweenOrderById_ReportTimeAsc(startDate: Date, endDate: Date) : List<AppStatistics>
+    fun findById_TypeEqualsAndId_ReportTimeBetweenOrderById_ReportTimeAsc(type: Int, startDate: Date,
+                                                                          endDate: Date) : List<AppStatistics>
 
-    fun findById_ReportTimeBetween(startDate: Date, endDate: Date) : List<AppStatistics>
 }
