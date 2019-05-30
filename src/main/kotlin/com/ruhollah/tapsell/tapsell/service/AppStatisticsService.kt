6589f9fb29eaea57e81b8bee29a9b15a868a1b5e
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2019-05-30 19:13:01 +0430

    Remove wrong annotations

--- src/main/kotlin/com/ruhollah/tapsell/tapsell/service/AppStatisticsService.kt
+++ src/main/kotlin/com/ruhollah/tapsell/tapsell/service/AppStatisticsService.kt
@@ -4,7 +4,7 @@ import com.ruhollah.tapsell.tapsell.response.AppStatisticsListResponse
 import org.springframework.stereotype.Service
 import java.util.*
 
-@Service
+
 interface AppStatisticsService
 {
     fun getStats(startDate: Date, endDate: Date, type: Int): AppStatisticsListResponse
