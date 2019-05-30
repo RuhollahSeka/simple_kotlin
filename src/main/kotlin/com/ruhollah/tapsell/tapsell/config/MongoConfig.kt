60c663b76af73a1733c07b95d821ed77e2afc022
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2019-05-30 15:12:59 +0430

    Update config

--- src/main/kotlin/com/ruhollah/tapsell/tapsell/config/MongoConfig.kt
+++ src/main/kotlin/com/ruhollah/tapsell/tapsell/config/MongoConfig.kt
@@ -1,34 +1,34 @@
 package com.ruhollah.tapsell.tapsell.config
 
-import com.ruhollah.tapsell.tapsell.document.AppStatistics
 import com.ruhollah.tapsell.tapsell.repository.AppStatisticsRepository
+import com.ruhollah.tapsell.tapsell.service.AppStatisticsService
 import org.springframework.boot.CommandLineRunner
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
-import java.text.DateFormat
 import java.text.SimpleDateFormat
-import java.util.*
 
 @Configuration
 @EnableMongoRepositories(basePackageClasses = [AppStatisticsRepository::class])
+@EnableCaching
 class MongoConfig
 {
 
     @Bean
-    fun commandLineRunner(appStatisticsRepository: AppStatisticsRepository): CommandLineRunner
+    fun commandLineRunner(appStatisticsService: AppStatisticsService): CommandLineRunner
     {
         return CommandLineRunner {
 //            val format = SimpleDateFormat("yyyy.MM.dd")
 //            val date = format.parse("2019.01.01")
-//            appStatisticsRepository.save(AppStatistics(AppStatistics.AppStatisticsKey("app4", date, 1),
+//            appStatisticsRepository.save(AppStatistics(AppStatistics.AppStatisticsKey("app4", date, 2),
 //                    1, 1, 1, 1, 1, 1))
             val format = SimpleDateFormat("yyyy.MM.dd")
             val startDate = format.parse("2020.01.01")
             val endDate = format.parse("2018.01.01")
-            val list = appStatisticsRepository.findById_ReportTimeBetweenOrderById_ReportTimeAsc(endDate, startDate)
-//            val list = appStatisticsRepository.findById_ReportTimeBetween(endDate, startDate)
-            println()
+            val list = appStatisticsService.getStats(endDate, startDate, 1)
+            list.stats.forEach { println("Year: ${it.year}, Week: ${it.weekNum}, Requests: ${it.requests}, " +
+                    "Clicks: ${it.clicks}, Installs: ${it.installs}") }
         }
     }
 
