c1f97c7c9c0b807161d0b64808036299434f050e
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2019-05-30 10:29:40 +0430

    Add AppStatistics

--- src/main/kotlin/com/ruhollah/tapsell/tapsell/document/AppStatistics.kt
+++ src/main/kotlin/com/ruhollah/tapsell/tapsell/document/AppStatistics.kt
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document
 import java.util.*
 
 @Document
-class AppStatistics(@Id val key: CompositeKey,
+class AppStatistics(@Id val id: AppStatisticsKey,
                     val videoRequests: Int,
                     val webViewRequest: Int,
                     val videoClicks: Int,
@@ -13,7 +13,23 @@ class AppStatistics(@Id val key: CompositeKey,
                     val videoInstalls: Int,
                     val webViewInstalls: Int)
 {
-    inner class CompositeKey(val id: String,
-                             val reportTime: Date,
-                             val type: Int)
+//    @Id
+//    val id: AppStatisticsKey
+//
+//    constructor(id: String, reportTime: Date, type: Int, videoRequests: Int, webViewInstalls: Int, videoClicks: Int,
+//                webViewClicks: Int, videoInstalls: Int, webViewInstalls: Int)
+//    {
+//        this.id = AppStatisticsKey(id, reportTime, type)
+//
+//    }
+//
+//    init
+//    {
+//        this.id = AppStatisticsKey(id, reportTime, type)
+//
+//    }
+
+    class AppStatisticsKey(val id: String,
+                           val reportTime: Date,
+                           val type: Int)
 }
