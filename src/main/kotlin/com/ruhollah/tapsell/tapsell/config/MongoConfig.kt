6354e5dc10ae874290f105dd24c8d27a57aaf961
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2019-05-31 09:14:01 +0430

    Clean MongoConfig Class

--- src/main/kotlin/com/ruhollah/tapsell/tapsell/config/MongoConfig.kt
+++ src/main/kotlin/com/ruhollah/tapsell/tapsell/config/MongoConfig.kt
@@ -13,23 +13,3 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableCaching
 @ComponentScan(basePackageClasses = [AppStatisticsServiceImpl::class, AppStatisticsController::class])
 class MongoConfig
-{
-//    @Bean
-//    fun commandLineRunner(appStatisticsService: AppStatisticsService,
-//                          appStatisticsRepository: AppStatisticsRepository): CommandLineRunner
-//    {
-//        return CommandLineRunner {
-////            val format = SimpleDateFormat("yyyy.MM.dd")
-////            val date = format.parse("2019.01.01")
-////            appStatisticsRepository.save(AppStatistics("app4", date, 2, 1, 1,
-////                    1, 1, 1, 1))
-//            val format = SimpleDateFormat("yyyy.MM.dd")
-//            val startDate = format.parse("2020.01.01")
-//            val endDate = format.parse("2018.01.01")
-//            val list = appStatisticsService.getStats(endDate, startDate, 2)
-//            list.stats.forEach { println("Year: ${it.year}, Week: ${it.weekNum}, Requests: ${it.requests}, " +
-//                    "Clicks: ${it.clicks}, Installs: ${it.installs}") }
-//        }
-//    }
-
-}
